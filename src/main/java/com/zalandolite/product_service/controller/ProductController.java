ef305ccc19916d782deafdfff86b5828ba8662ea
Ornella <ornelladelvicario@gmail.com>
2025-08-07 16:04:47 +0200

    feat(product-service): add Product entity, repository and controller

---
+++ src/main/java/com/zalandolite/product_service/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.zalandolite.product_service.controller;
+
+import com.zalandolite.product_service.model.Product;
+import com.zalandolite.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody Product product) {
+        productRepository.save(product);
+    }
+}
